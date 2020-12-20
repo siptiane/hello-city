aa5f989760769495da82ccbb1f93f9e58d5199f2
siptiane kulapa <sebastienkulapa@gmail.com>
2020-12-20 19:13:02 +0100

    Do not display about link on about page

--- resources/views/pages/about-as.blade.php
+++ resources/views/pages/about-as.blade.php
@@ -5,6 +5,6 @@
     
         @section('content')
             <p>Construit avec &hearts; par SIPTIANE KULAPA.</p>
-            <a href="/">Revenir a la page d'accueil</a>
+            <a href="{{route('home')}}">Revenir a la page d'accueil</a>
         @endsection
      
--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('pages/home');
-});
+})->name('home');
 
 Route::get('/about-as', function () {
     return view('pages/about-as');
-});
+})->name('about');
